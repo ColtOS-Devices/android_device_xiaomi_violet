dfe8c9e3ba13f0e31c812e185f6dd8c6c68416d3
Ashwin R C <ashwin2001achu@gmail.com>
2020-01-09 18:22:40 +0000

    violet: move build fp back to makefile

--- init/init_violet.cpp
+++ init/init_violet.cpp
@@ -57,7 +57,5 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 
 void vendor_load_properties()
 {
-    // fingerprint
-    property_override("ro.build.description", "violet-user 9 PKQ1.181203.001 V10.3.9.0.PFHINXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.200205.002/6084387:user/release-keys");
+
 }
